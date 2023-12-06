9ab30d560162d261217e342a03f6ba63ebfa1e53
tomsa9 <tomsa@wix.com>
2023-12-06 16:38:46 +0200

    Reduce function even more

--- day6/part2.js
+++ day6/part2.js
@@ -2,10 +2,8 @@ const fs = require('fs');
 
 const filePath = 'input1';
 const numberRegex = /\d+/g
-const input= fs.readFileSync(filePath).toString().split('\n').map(x => x.substring(x.indexOf(':') + 1))
-
-const race = parseInt(input[0].replaceAll(' ', ''))
-const distance = parseInt(input[1].replaceAll(' ', ''))
+const [race, distance]= fs.readFileSync(filePath).toString().split('\n').map(x => x.substring(x.indexOf(':') + 1))
+    .map(x => parseInt(x.replaceAll(' ', '')))
 
 let acc = 0
 for (let i = 0; i <= race; i++) {
